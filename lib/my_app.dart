349696850f1bcabb45ba5c085ab4571e6828d23d
Muhammad Shehzad <shezikhan2014pk@gmail.com>
2026-03-09 13:15:13 +0500

    in this branch i add the kalma repo and run the project

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:testing_apis_of_imagify_app/view/Auth/Login_screen.dart';
+import 'package:testing_apis_of_imagify_app/view/kalmaScreen/kamla_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: KamlaScreen());
   }
 }
--- lib/res/app_url.dart
+++ lib/res/app_url.dart
@@ -1,3 +1,4 @@
 class AppUrl {
   static var baseUrl = 'https://api.zaheen.com.pk/api/';
+  static var kamlaApiEndPoint = '$baseUrl+chapter/93/videos';
 }
