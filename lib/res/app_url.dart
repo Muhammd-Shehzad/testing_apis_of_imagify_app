6408753faefffaca9e5c238ce14545e892892404
Muhammad Shehzad <shezikhan2014pk@gmail.com>
2026-03-10 11:52:06 +0500

    only remain in this  branch is show the data in the ui

--- lib/res/app_url.dart
+++ lib/res/app_url.dart
@@ -1,6 +1,6 @@
 class AppUrl {
   // static var baseUrl = 'https://api.zaheen.com.pk/api/';
-  static var baseUrl = 'http://api.alquran.cloud/v1/';
+  static var baseUrl = 'http://api.alquran.cloud/v1';
   static var kamlaApiEndPoint = '$baseUrl+chapter/93/videos';
   static var quranEndPoint = '$baseUrl+/ar.asad';
 }
